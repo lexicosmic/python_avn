7d9d6402d879d24364f879c540b455b41197d27e
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-08-19 00:23:33 -0300

    Adiciona título e legendas

--- script.py
+++ script.py
@@ -2,8 +2,15 @@
 
 import matplotlib.pyplot as plt
 
-x = [1, 2]
-y = [2, 3]
+x = [1, 2, 5]
+y = [2, 3, 7]
+
+# Título
+plt.title("Meu primeiro grafico com Python")
+
+# Eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
